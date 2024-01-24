f20f377dde736eace89be36fccee29b3094d3e1e
conditional <105pkn@mail.ru>
2024-01-24 21:51:57 +0700

    добавил в константы в services

---
+++ src/main/java/ru/skypro/telegrambotdogshelter/services/Const.java
@@ -0,0 +1,41 @@
+package ru.skypro.telegrambotdogshelter.services;
+
+public class Const {
+    public static final String INTRODUCE_RULES = "Правила знакомства с животным до того, как забрать его из приюта";
+    public static final String DOCUMENT_LIST =
+            "список документов, необходимых для того, чтобы взять животное из приюта";
+    public static final String LIST_TRANSPORTATION_RECOMMENDATIONS =
+            "список рекомендаций по транспортировке животного";
+    public static final String LIST_RECOMMENDATIONS_FOR_ARRANGING_HOME_FOR_PUPPY
+            = "список рекомендаций по обустройству дома для щенка";
+    public static final String LIST_RECOMMENDATIONS_FOR_ARRANGING_HOME_FOR_ADULT_ANIMAL
+            = "список рекомендаций по обустройству дома для взрослого животного";
+    public static final String LIST_RECOMMENDATIONS_FOR_ARRANGING_HOME_FOR_ANIMAL_WITH_DISABILITIES
+            = "список рекомендаций по обустройству дома для животного с ограниченными возможностями (зрение, передвижение)";
+    public static final String ADVICE_FROM_DOG_HANDLER_ON_INITIAL_COMMUNICATION_WITH_DOG
+            = "советы кинолога по первичному общению с собакой";
+    public static final String LIST_REASONS_FOR_REFUSAL
+            = "список причин, почему могут отказать и не дать забрать собаку из приюта";
+    public static final String WRITE_DOWN_CONTACT_DETAILS
+            = "принять и записать контактные данные для связи";
+    public static final String CALL_VOLUNTEER
+            = "позвать волонтера";
+    public static final String SEND_DAILY_REPORT_FORM
+            = "прислать форму ежедневного отчета";
+    public static final String REQUEST_TEXT
+            = "пользователь прислал только фото, бот может запросить текст";
+    public static final String REQUEST_PHOTO
+            = "пользователь прислал только текст, бот может запросить фото";
+    public static final String WARNING_ABOUT_REPORT
+            = "Дорогой усыновитель, мы заметили, что ты заполняешь отчет не так " +
+            "подробно, как необходимо. Пожалуйста, подойди ответственнее к этому занятию. " +
+            "В противном случае волонтеры приюта будут обязаны самолично проверять условия содержания животного";
+    public static final String CONGRATULATIONS_PROBATIONARY_PERIOD_HAS_PASSED
+            = "Поздравляем! Испытательный срок пройден!";
+    public static final String YOU_HAVE_BEEN_ASSIGNED_ADDITIONAL_PROBATIONARY_TIME
+            = "Вам назначено дополнительное время испытательного срока";
+    public static final String ADOPTIVE_PARENT_NOT_PASS_PROBATIONARY_PERIOD
+            = "Усыновитель не прошел испытательный срок";
+    public static final String RECOMMENDATIONS_ABOUT_SAFETY_PRECAUTIONS_ON_TERRITORY_SHELTER
+            = "общие рекомендации о технике безопасности на территории приюта";
+}
