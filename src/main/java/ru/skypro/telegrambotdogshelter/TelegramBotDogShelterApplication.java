782923b8fd971414d8eed8a8af85a9aaad4c1463
Kseniia313 <kshome@mail.ru>
2024-02-22 21:54:49 +0300

    Коммит для cherry-pick(промежуточный коммит)

--- src/main/java/ru/skypro/telegrambotdogshelter/TelegramBotDogShelterApplication.java
+++ src/main/java/ru/skypro/telegrambotdogshelter/TelegramBotDogShelterApplication.java
@@ -1,10 +1,11 @@
 package ru.skypro.telegrambotdogshelter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@OpenAPIDefinition
 public class TelegramBotDogShelterApplication {
 
     public static void main(String[] args) {
